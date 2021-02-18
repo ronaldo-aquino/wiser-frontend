243290a552737b1793dc0a84236d5c71ff052667
Ronaldo Aquino <ronaldoaquinodev@gmail.com>
2021-02-17 21:55:39 -0300

    Projeto inicializado

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Login App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main>
+        <h1 style={{fontFamily: 'Montserrat'}}>Olá mundo</h1>
+      </main>
+    </div>
+  );
+}
